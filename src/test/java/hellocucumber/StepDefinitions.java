867a1bfae8c30032f2ff1d8e3ebe2651da89bcca
Enrique Riesgo <enrique.riesgo@gmail.com>
2024-02-16 16:38:53 +0000

    Finish guided tutorial

---
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,47 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions.*;
+
+public class StepDefinitions {
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+    @Given("today is {string}")
+    public void today_is(String day) {
+        this.today = day;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
